49b48cbbcc8f8a2c7b30721312182afd6c27a6a5
IrfanRomadhonWidodo <irfan.widodo@mhs.unsoed.ac.id>
2024-11-24 00:42:26 +0700

    progress

--- JAVASCRIPT/home2_theseven.js
+++ JAVASCRIPT/home2_theseven.js
@@ -22,3 +22,23 @@ function resetMenu() {
     hamburger.style.transform = 'rotate(0deg)';
 }
 
+function setupObserver(selector) {
+    const element = document.querySelector(selector);
+    if (element) {
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible'); // Tambah class "visible" untuk memulai animasi
+            observer.unobserve(entry.target); // Hentikan pengamatan
+          }
+        });
+      }, {
+        threshold: 0.5 // 50% elemen terlihat
+      });
+      observer.observe(element); // Mulai pengamatan
+    }
+  }
+  
+  // Tambahkan pengamatan untuk elemen tertentu
+  setupObserver('.vought-logo-2');
+  
