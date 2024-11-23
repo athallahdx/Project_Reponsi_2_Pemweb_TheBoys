ee4f5c522413da75dc0edef9a2a6e79598c4ff6d
IrfanRomadhonWidodo <irfan.widodo@mhs.unsoed.ac.id>
2024-11-23 22:27:08 +0700

    coba malam

---
+++ JAVASCRIPT/home2_theseven.js
@@ -0,0 +1,25 @@
+
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+}
+
+// Tambahkan event listener untuk semua link
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        const navLinks = document.querySelector('.nav-links');
+        navLinks.classList.remove('show'); // Tutup menu setelah link diklik
+    });
+});
+
+function hoverMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.style.transform = 'rotate(0deg)';
+}
+
+function resetMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.style.transform = 'rotate(0deg)';
+}
+
